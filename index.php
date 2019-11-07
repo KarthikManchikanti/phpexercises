67446ffddc45856095cfeadcae8295a62b476626
Karthik Manchikanti <kmanchik@isimtl.local>
2019-11-07 16:07:36 -0500

    Hello

---
+++ index.php
@@ -0,0 +1,39 @@
+
+   <?php
+// myvar = 0;   the variable does not start with the $ character
+
+
+
+ $myvar = 0;
+ var_dump($myvar);
+ 
+ $var5 = 0;
+ var_dump($var5);
+ 
+ $_myvar = 0;
+ var_dump($_myvar);
+
+
+ $_5var = 0;
+ var_dump($_5var);
+ 
+ 
+ $__element1 = 0;
+ var_dump($__element1);
+ 
+ 
+ //$age* = 0;  There shouldnt be star 
+ 
+$name='Karthik';
+var_dump($name);
+
+$firstname='Manchikanti';
+var_dump($firstname);
+
+$age=24;
+var_dump($age);
+
+$city="Hyderabad";
+var_dump($city);
+ 
+?>
